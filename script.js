f4b6661e6486642bbc00db91ba86cf13773090b3
Karthi <KarthihaKrishnan@KarthihaKrishnan@users.noreply.github.com.>
2025-08-22 12:40:51 +1000

    Add function to generate human choice (rock, paper, scissors)

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 console.log("Hellow World!"); //script check
 
 // Write the logic to get computer choice
-
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-
 console.log(getComputerChoice());
+
+//Write the logic to get human choice
+function getHumanChoice() {
+    const input = prompt("Enter Rock, Paper, and Scissors:");
+    return input.trim().toLowerCase(); //Case-insensitive
+}
+console.log(getHumanChoice());
